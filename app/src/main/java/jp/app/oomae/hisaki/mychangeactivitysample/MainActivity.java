1e00eb85a38aa8c41441035412e232097f483542
hisaki0616 <hisaki0616@gmail.com>
2017-08-27 12:08:29 +0900

    renew

--- app/src/main/java/jp/app/oomae/hisaki/mychangeactivitysample/MainActivity.java
+++ app/src/main/java/jp/app/oomae/hisaki/mychangeactivitysample/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.viewholder_item);
 
         findViewById(R.id.button1).setOnClickListener(changeActivity);
     }
